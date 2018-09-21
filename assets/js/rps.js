2ce1ae9d4281656dce2044e98aa1ed8c3e99bffb
Bryan Magers <41593930+bryanmagers@users.noreply.github.com>
2018-09-21 14:13:54 -0700

    initial files

---
+++ assets/js/rps.js
@@ -0,0 +1,20 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyB4tq1kGzhERV2pDV4s_yBdsc3_Ir8Uh24",
+authDomain: "rockpaperscissors-9fef3.firebaseapp.com",
+databaseURL: "https://rockpaperscissors-9fef3.firebaseio.com",
+projectId: "rockpaperscissors-9fef3",
+storageBucket: "rockpaperscissors-9fef3.appspot.com",
+messagingSenderId: "334586570997"
+};
+firebase.initializeApp(config);
+
+$(document).ready(function() {
+
+
+
+
+
+
+
+});
