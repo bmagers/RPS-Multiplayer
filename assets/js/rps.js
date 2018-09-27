a401db13bcc3ea450a8372ce4c8a622ada71d409
Bryan Magers <bmagers@users.noreply.github.com>
2018-09-27 14:00:18 -0700

    change the way turns are handled

--- assets/js/rps.js
+++ assets/js/rps.js
@@ -10,8 +10,18 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 var currentUser;
-var whoseTurn = 1;
+var playing;
+var waiting;
 var localPlayer;
+resetGame();
+
+function resetGame() {
+  database.ref("/turn").set({
+    player: 1
+  });
+  playing = 1;
+  waiting = 2;
+}
 
 function addUser(name) {
   var playerToAdd;
@@ -28,10 +38,6 @@ function addUser(name) {
     }
   });
   // add the player to Firebase
-  var turn = false;
-  if (playerToAdd === 1) {
-    turn = true;
-  }
   if (playerToAdd != 0) {
     database.ref("/players/" + playerToAdd).set({
       name: name,
@@ -39,7 +45,6 @@ function addUser(name) {
       losses: 0,
       ties: 0,
       implement: "",
-      turn: turn,
       connected: true
     });
     var ref = database.ref("/players/" + playerToAdd + "/connected");
@@ -48,62 +53,45 @@ function addUser(name) {
   }
 }
 
+function buttons() {
+  var button1 = $("<button>").addClass("btn btn-primary implementButton").attr("id", "ROCK").text("ROCK");
+  var button2 = $("<button>").addClass("btn btn-primary implementButton").attr("id", "PAPER").text("PAPER");
+  var button3 = $("<button>").addClass("btn btn-primary implementButton").attr("id", "SCISSORS").text("SCISSORS");
+  var implementsDiv = $("#implements" + localPlayer).append(button1).append(button2).append(button3);
+  $("#player" + localPlayer).append(implementsDiv);
+}
+
+function switchTurn() {
+  database.ref("/turn").set({
+    player: waiting
+  });
+}
+
 function updatePlayer(snapshot) {
   for (var i = 1; i <= 2; i++) {
-    if (snapshot.child(i).exists()) {
+      if (snapshot.child(i).exists()) {
       // common to both players
       var displayName = $("<h1>").text(snapshot.val()[i].name);
       var displayStats = $("<h2>").text(snapshot.val()[i].wins + "-" + snapshot.val()[i].losses + "-" + snapshot.val()[i].ties);
       $("#player" + i).html(displayName).append(displayStats);
-
-      // local player only
-      if (i === localPlayer && snapshot.val()[i].turn) {
-        var button1 = $("<button>").addClass("btn btn-primary implementButton").attr("id", "rock").text("ROCK");
-        var button2 = $("<button>").addClass("btn btn-primary implementButton").attr("id", "paper").text("PAPER");
-        var button3 = $("<button>").addClass("btn btn-primary implementButton").attr("id", "scissors").text("SCISSORS");
-        $("#player" + i).append(button1).append(button2).append(button3);
-        // if (snapshot.val()[i].turn) {
-        //   $("#turnStatus").text("It's your turn!");
-        // } else {
-        //   $("#turnStatus").text("Waiting for your opponent.");
-        // }
-      } else {
-        // remote player disconnected
-        if (!snapshot.val()[i].connected) {
-          $("#turnStatus").text("The remote player disconnected.");
-          $("#player" + i).html("Waiting for Player " + i);
-          database.ref("/players/" + i).remove();
-
+      // add RPS buttons
+      var implementsDiv = $("<div>").attr("id", "implements" + i);
+      var implement = snapshot.val()[i].implement;
+      if (implement === "") {
+        if (i === localPlayer && i === playing) {
+          buttons();
         }
+      } else {
+        $(implementsDiv).text(implement);
       }
+      if (i !== localPlayer && !snapshot.val()[i].connected) {
+        $("#turnStatus").text("The remote player disconnected.");
+        $("#player" + i).html("Waiting for Player " + i);
+        database.ref("/players/" + i).remove();
+        resetGame();
+      }
+      $("#player" + i).append(implementsDiv);
     }
-  }
-}
-
-function switchTurn() {
-  if (whoseTurn === 1) {
-    database.ref("/players/1").update({
-      turn: false
-    });
-    database.ref("/players/2").update({
-      turn: true
-    });
-    $("#player1").css("border-color", "gray");
-    $("#player2").css("border-color", "yellow");
-  } else {
-    database.ref("/players/1").update({
-      turn: true
-    });
-    database.ref("/players/2").update({
-      turn: false
-    });
-    $("#player1").css("border-color", "yellow");
-    $("#player2").css("border-color", "gray");
-  }
-  if (whoseTurn === 1) {
-    whoseTurn = 2;
-  } else {
-    whoseTurn = 1;
   }
 }
 
@@ -114,7 +102,6 @@ $(document).ready(function() {
   $(button).on("click", function() {
     currentUser = $(input).val().trim();
     $(button).off();
-    console.log("current user is " + currentUser);
     $("#turnStatus").text("Waiting for player 2");
     addUser(currentUser);
   });
@@ -123,7 +110,11 @@ $(document).ready(function() {
     database.ref("/players/" + localPlayer).update({
       implement: this.id
     });
-    if (whoseTurn === 1) {
+    console.log("#implements" + playing);
+    $("#implements" + playing).text(this.innerText);
+
+
+    if (playing === 1) {
       //
     } else {
       // waiting for #1
@@ -142,5 +133,14 @@ $(document).ready(function() {
     // update player info
     updatePlayer(snapshot);
   });
+
+  database.ref("/turn").on("value", function(snapshot) {
+    // switch turn
+    playing = snapshot.val().player;
+    waiting = (playing === 1) ? 2 : 1;
+    $("#player" + waiting).css("border-color", "gray");
+    $("#player" + playing).css("border-color", "yellow");
+    buttons();
+  })
 
 });
